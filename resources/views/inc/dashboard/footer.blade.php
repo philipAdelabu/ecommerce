ced153ac85de350f76f70f3b3425389d0e5b31fc
Philip Adelabu <adebisiphilip@gmail.com>
2025-06-23 16:02:56 +0100

    added some other admin dashboard files

---
+++ resources/views/inc/dashboard/footer.blade.php
@@ -0,0 +1,15 @@
+        
+     </div>
+    </div>
+    <div class="sidebar-overlay" data-reff=""></div>
+    <script src="{{ url('assets/js/jquery-3.2.1.min.js') }}"></script>
+	<script src="{{ url('assets/js/popper.min.js') }}"></script>
+    <script src="{{ url('assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{ url('assets/js/jquery.slimscroll.js') }}"></script>
+    <script src="{{ url('assets/js/Chart.bundle.js') }}"></script>
+    <script src="{{ url('assets/js/chart.js') }}"></script>
+    <script src="{{ url('assets/js/app.js') }}"></script>
+
+</body>
+
+</html>
